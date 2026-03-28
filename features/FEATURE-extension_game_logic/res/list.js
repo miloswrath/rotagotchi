5089564b3e0748c43563ae76f86957e4d94dc0df
Zak Gilliam <zakgilliam@gmail.com>
2026-03-27 19:34:41 -0500

    docs: initial feature spec

---
+++ features/FEATURE-extension_game_logic/res/list.js
@@ -0,0 +1,4 @@
+const whitelist = ["youtube.com", "instagram.com", "facebook.com", "tiktok.com", "twitter.com"]; // Should have a bunch of common social media sites and entertainment sites here.
+const blacklist = ["outlook.com", "google.com", "github.com", "linkedin.com"]; // Should have a bunch of common work sites here.
+
+export { whitelist, blacklist };
